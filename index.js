071209d0cb3be1b5ff150bb4444337034b46909a
mirajul alam <alifmirajulalam@gmail.comm>
2022-05-23 21:49:33 +0600

    specific user purchase item show get oparation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
@@ -10,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = `mongodb+srv://car_parts:I1okgsomsKk3gD4t@cluster0.epu3g.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.epu3g.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -19,6 +20,7 @@ async function run() {
     try {
         await client.connect();
         const productsCollection = client.db('car_parts').collection('product');
+        const ordersCollection = client.db('car_parts').collection('order');
 
         // handle get all product load
         app.get('/product', async (req, res) => {
@@ -35,6 +37,20 @@ async function run() {
             const products = await productsCollection.findOne(query)
             res.send(products)
         });
+
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const orders = await ordersCollection.find(query).toArray();
+            res.send(orders)
+        })
+
+        // products order successful
+        app.post("/order", async (req, res) => {
+            const product = req.body;
+            const result = await ordersCollection.insertOne(product)
+            res.send(result)
+        })
     }
     finally {
 
